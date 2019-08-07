f87fd1f844871182928eaf2678d3e0ac582819b8
Tomek <moraviec@gmail.com>
2019-08-07 13:21:32 +0200

    seeds for users and permission policy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    PermissionPolicy.create!([
+        {name: 'pełny dostęp', io1: true, io2: true, io3: true, io4: true},
+        {name: 'brak dostępu', io1: false, io2: false, io3: false, io4: false}
+    ])
+
+    User.create!([
+        {email: "user1@example.com", password: "12345678", password_confirmation: "12345678", admin: true, permission_policy_id: 1},
+        {email: "user2@example.com", password: "12345678", password_confirmation: "12345678", admin: false, permission_policy_id: 2},
+        {email: "user3@example.com", password: "12345678", password_confirmation: "12345678", admin: false, permission_policy_id: 2},
+        {email: "user4@example.com", password: "12345678", password_confirmation: "12345678", admin: false, permission_policy_id: 2},
+    ])
